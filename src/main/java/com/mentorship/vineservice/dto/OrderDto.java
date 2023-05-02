963282d63538b60f36c246107ccd2d49ae76cd6e
volodymyry-backbase <volodymyry@backbase.com>
2023-05-02 18:50:53 +0300

    fix comments

--- src/main/java/com/mentorship/vineservice/dto/OrderDto.java
+++ src/main/java/com/mentorship/vineservice/dto/OrderDto.java
@@ -36,8 +36,4 @@ public class OrderDto {
         private Integer vineAmount;
 
     }
-
-
-
-
 }
--- src/main/java/com/mentorship/vineservice/repository/VineRepository.java
+++ src/main/java/com/mentorship/vineservice/repository/VineRepository.java
@@ -1,11 +1,9 @@
 package com.mentorship.vineservice.repository;
 
 import com.mentorship.vineservice.domain.Vine;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface VineRepository extends JpaRepository<Vine, Long>, JpaSpecificationExecutor<Vine> {
 
-    Optional<Vine> findById(Long vineId);
 }
